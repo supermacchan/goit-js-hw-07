eddd73e935fbdb082a06a9fbbd7cb826421d3ddf
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-15 21:31:48 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,16 +3,39 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryList = galleryItems.map(item => {
+    const listItem = document.createElement('div');
+    listItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = item.original;
+
+    const galleryPic = document.createElement('img');
+    galleryPic.classList.add('gallery__image');
+    galleryPic.src = item.preview;
+    galleryPic.dataset.source = item.original;
+    galleryPic.alt = item.description;
+    
+    listItem.append(galleryLink);
+    galleryLink.append(galleryPic);
+
+    // console.log(listItem); 
+
+    return listItem;
+});
+
+const galleryParent = document.querySelector('.gallery');
+galleryParent.append(...galleryList);
 
 
-{/* <li>
-    <a>
-        <img />
-    </a>
-</li> */}
+// добавить в делегирование снятие дефотла?
+// const galleryLink = document.querySelectorAll('.gallery__link');
+// galleryLink.addEventListener('click', (event) => {
+//     event.preventDefault();
+// })
 
-// Создание и рендер разметки по массиву данных galleryItems
-// и предоставленному шаблону элемента галереи.
+
 
 // Реализация делегирования на div.gallery и получение url большого изображения.
 
