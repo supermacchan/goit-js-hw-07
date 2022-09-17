3ef01473d23145f12d13746e8cc62aff40c52ae2
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-17 16:24:47 +0300

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,8 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryParent = document.querySelector('.gallery');
+
 const galleryList = galleryItems.map(item => {
     const listItem = document.createElement('div');
     listItem.classList.add('gallery__item');
@@ -20,31 +22,30 @@ const galleryList = galleryItems.map(item => {
     listItem.append(galleryLink);
     galleryLink.append(galleryPic);
 
-    // console.log(listItem); 
-
     return listItem;
 });
 
-const galleryParent = document.querySelector('.gallery');
 galleryParent.append(...galleryList);
 
+galleryParent.addEventListener('click', onImageClick);
 
-// добавить в делегирование снятие дефотла?
-// const galleryLink = document.querySelectorAll('.gallery__link');
-// galleryLink.addEventListener('click', (event) => {
-//     event.preventDefault();
-// })
+function onImageClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
 
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img class="modal__image" src="" alt="" />
+        </div>
+    `)
 
+    instance.show();
 
-// Реализация делегирования на div.gallery и получение url большого изображения.
+    const modalImage = document.querySelector('.modal__image');
+    modalImage.src = e.target.dataset.source;
+    modalImage.alt = e.target.alt;
+    
+}
 
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
-// Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) 
-// файлы библиотеки.
-
-// Открытие модального окна по клику на элементе галереи.
-// Для этого ознакомься с документацией и примерами.
-
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием.
-// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
